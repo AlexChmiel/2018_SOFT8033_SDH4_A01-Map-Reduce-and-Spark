3aedc17d06dd249eff5785554d579db27f2dc7d9
Aleksander Chmiel <aleksander.chmiel@mycit.ie>
2018-03-07 17:03:10 +0000

    Implemented my_mapper for Hint2

---
+++ Hint2/Part1/my_mapper.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+# --------------------------------------------------------
+#           PYTHON PROGRAM
+# Here is where we are going to define our set of...
+# - Imports
+# - Global Variables
+# - Functions
+# ...to achieve the functionality required.
+# When executing > python 'this_file'.py in a terminal,
+# the Python interpreter will load our program,
+# but it will execute nothing yet.
+# --------------------------------------------------------
+
+import codecs
+import sys
+
+
+# ------------------------------------------
+# FUNCTION get_total_visits
+# ------------------------------------------
+def get_total_visits(input_stream):
+    # Get the total number of visits per each page.
+    total = 0
+    
+    for each_line in input_stream:
+        words = each_line.split()
+        # Check if the string at position 2 is a digit, before being converted to integer.
+        # This threw errors when special characters were involved in one of the files.
+        if words[2].isdigit():
+            visits = int(words[2])
+            total = total + visits
+        
+    return total
+
+# ------------------------------------------
+# FUNCTION my_map
+# ------------------------------------------
+def my_map(input_stream, output_stream):
+    total_visits = get_total_visits(input_stream)
+    str_returned = "Total visits\t" + str(total_visits) + "\n"
+    output_stream.write(str_returned)
+# ------------------------------------------
+# FUNCTION my_main
+# ------------------------------------------
+def my_main(debug, i_file_name, o_file_name):
+    # We pick the working mode:
+
+    # Mode 1: Debug --> We pick a file to read test the program on it
+    if debug == True:
+        my_input_stream = codecs.open(i_file_name, "r", encoding='utf-8')
+        my_output_stream = codecs.open(o_file_name, "w", encoding='utf-8')
+    # Mode 2: Actual MapReduce --> We pick std.stdin and std.stdout
+    else:
+        my_input_stream = sys.stdin
+        my_output_stream = sys.stdout
+
+    # We launch the Map program
+    my_map(my_input_stream, my_output_stream)
+
+# ---------------------------------------------------------------
+#           PYTHON EXECUTION
+# This is the main entry point to the execution of our program.
+# It provides a call to the 'main function' defined in our
+# Python program, making the Python interpreter to trigger
+# its execution.
+# ---------------------------------------------------------------
+if __name__ == '__main__':
+    # 1. Input parameters
+    debug = True
+
+    i_file_name = "pageviews-20180219-100000_18.txt"
+    o_file_name = "mapResult.txt"
+
+    # 2. Call to the function
+    my_main(debug, i_file_name, o_file_name)
